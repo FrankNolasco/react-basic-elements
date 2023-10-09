15719e2500f2123e75b61799b87feb3f47fcf94b
Frank Nolasco Velasquez <49081234+FrankNolasco@users.noreply.github.com>
2023-10-08 22:57:55 -0500

    remove dist & add yaml

--- dist/stories/molecules/DisplaySelector/DisplaySelector.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface IDispSelect {
-    type: "grid" | "flex" | "column" | "row" | "row-wrap" | undefined;
-    gap?: string;
-    cols?: number;
-    children?: React.ReactNode;
-}
-export declare const DisplaySelector: React.FC<IDispSelect>;
-//# sourceMappingURL=DisplaySelector.d.ts.map

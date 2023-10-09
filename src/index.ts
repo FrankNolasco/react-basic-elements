94ea9ece2fb59f3bb38d599e4a656d1a1b899bc2
Frank Nolasco Velasquez <49081234+FrankNolasco@users.noreply.github.com>
2023-10-08 23:08:47 -0500

    remove actions for now

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,13 @@ import { Grid, GridChild } from "./stories/atoms/Grid/Grid";
 import { Typography } from "./stories/atoms/Typography/Typography";
 import { DisplaySelector } from "./stories/molecules/DisplaySelector/DisplaySelector";
 
-export { Box, Column, Flex, Row, Grid, GridChild, Typography, DisplaySelector };
+export default {
+  Box,
+  Column,
+  Flex,
+  Row,
+  Grid,
+  GridChild,
+  Typography,
+  DisplaySelector,
+};
